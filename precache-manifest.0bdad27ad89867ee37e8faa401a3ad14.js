d2e111ba1e1e8aae443460566ee0c97e0ec0f458
Olesia <olesia.tk@gmail.com>
2021-01-20 13:03:43 +0200

    Updates

---
+++ precache-manifest.0bdad27ad89867ee37e8faa401a3ad14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b9db34b3ab074407bf1c23deec2eb6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "da9b4d39c99f73d83637",
+    "url": "./static/css/main.464c2f5a.chunk.css"
+  },
+  {
+    "revision": "eb26bc6216c02cfe5826",
+    "url": "./static/js/2.def181fb.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.def181fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9b4d39c99f73d83637",
+    "url": "./static/js/main.25fd706f.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.0bdad27ad89867ee37e8faa401a3ad14.js"
 );
 
 self.addEventListener('message', (event) => {
